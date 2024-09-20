6b0cb45987806efcb59b7363569669992d51c695
CodeJonDav <jonathan.d.sutton2024@gmail.com>
2024-09-19 21:14:31 -0700

    removed lines that are for future additions will work on a new branch

--- tenmo-client/src/main/java/com/techelevator/tenmo/util/HTTPEntityGenerator.java
+++ tenmo-client/src/main/java/com/techelevator/tenmo/util/HTTPEntityGenerator.java
@@ -19,14 +19,9 @@ public class HTTPEntityGenerator {
         return new HttpEntity<>(body, genAuthHead());
     }
 
-    public static HttpEntity<byte[]> generatedFileEntity(byte[] filecontent, String filename){
-        HttpHeaders headers = genAuthHead();
-        headers.setContentType(org.springframework.http.MediaType.MULTIPART_FORM_DATA);
-    }
-
-    private static HttpHeaders genAuthHead(){
+    private static HttpHeaders genAuthHead() {
         HttpHeaders headers = new HttpHeaders();
         headers.setBearerAuth(token);
-        return  headers;
+        return headers;
     }
 }
